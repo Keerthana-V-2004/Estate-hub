e3f2b474d4478beee45515e0422c98d37e172708
Keerthana V <keertanv1975@gmail.com>
2026-02-17 10:50:14 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,11 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log('Connected to mongodb');
+}).catch((err) => {
+  console.log(err.message);
+})
 const app = express();
 
 app.listen(3000 , () => {
